0ee72b76fc141fe5986590f12ef524a749eca704
David Berglin <david.berglin@gmail.com>
2025-11-28 08:25:40 +0100

    update

--- myFirstRazorPage/obj/Debug/net9.0/myFirstRazorPage.AssemblyInfo.cs
+++ myFirstRazorPage/obj/Debug/net9.0/myFirstRazorPage.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myFirstRazorPage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+672778e091be4e27a5a5d67585399fa2647f235a")]
 [assembly: System.Reflection.AssemblyProductAttribute("myFirstRazorPage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myFirstRazorPage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
